745c625589a686db48ebaa4974b3a893c27a5b25
Mostak Ahamed Nishat <37589801+Mostak-Ahamed-Nishat@users.noreply.github.com>
2022-04-13 12:59:01 +0600

    scrool behaviour & sticky bar added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,55 @@ const header = document.querySelector(".header");
 navBtn.addEventListener("click", function(e) {
     header.classList.toggle("nav-open")
 })
+
+//Smooth Scrolling
+
+const allLink = document.querySelectorAll("a:Link")
+
+allLink.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+
+        const href = link.getAttribute("href");
+
+        console.log(href)
+            // Scroll back to top
+        if (href === "#")
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+
+        if (href !== "#" && href.startsWith("#")) {
+            const section = document.querySelector(href)
+            section.scrollIntoView({
+                behavior: "smooth"
+            })
+        }
+
+    })
+})
+
+
+// Sticky Navigation
+
+
+const heroSection = document.querySelector('.section-hero');
+
+const observer = new IntersectionObserver(entries => {
+
+    const ent = entries[0]
+    if (!ent.isIntersecting) {
+        document.body.classList.add('sticky')
+    }
+    if (ent.isIntersecting) {
+        document.body.classList.remove('sticky')
+    }
+}, {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px'
+})
+
+
+observer.observe(heroSection)
